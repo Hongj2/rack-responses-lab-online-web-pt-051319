5a0288252187fe629f06f33ff8d7cb23152b2312
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-08-10 16:40:58 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     
     if Time.now.hour.between?(0,11)
       resp.write "Good Morning!"
-    else Time.now.hour.between?(12,24)
+    elsif Time.now.hour.between?(12,24)
       resp.write "Good Afternoon!"
     end
     
